91d48c24ca2b0c7d70b8556d69fc27b0e5301b16
Fuwn <contact@fuwn.me>
2023-12-20 14:35:41 -0800

    feat(dwm.c): bar height patch

--- config.h
+++ config.h
@@ -35,6 +35,8 @@ static int smartgaps =
     0;                  /* 1 means no outer gap when there is only one window */
 static int showbar = 1; /* 0 means no bar */
 static int topbar = 1;  /* 0 means bottom bar */
+static const int user_bh = 0;  /* 0 means that dwm will calculate bar height, >=
+                                  1 means dwm will user_bh as bar height */
 static const int vertpad = 10; /* vertical padding of bar */
 static const int sidepad = 30; /* horizontal padding of bar */
 /* static char *fonts[]          = { "monospace:size=10",
--- dwm.c
+++ dwm.c
@@ -1996,7 +1996,7 @@ void setup(void) {
   if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
     die("no fonts could be loaded.");
   lrpad = drw->fonts->h;
-  bh = drw->fonts->h + 2;
+  bh = user_bh ? user_bh : drw->fonts->h + 2;
   updategeom();
   sp = sidepad;
   vp = (topbar == 1) ? vertpad : -vertpad;
