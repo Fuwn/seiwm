a595c03ca72de42e1669b1d181978d082a969f4a
Fuwn <contact@fuwn.me>
2024-05-23 15:20:33 -0700

    feat(config.h): add bar height patch

--- config.h
+++ config.h
@@ -24,6 +24,7 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
 static const int vertpad            = 10;       /* vertical padding of bar */
 static const int sidepad            = 30;       /* horizontal padding of bar */
 static char *fonts[]          = { "Sarasa Term SC Nerd:size=11", "NotoColorEmoji:pixelsize=10:antialias=true:autohint=true"  };
--- dwm.c
+++ dwm.c
@@ -2045,7 +2045,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 2;
+	bh = drw->fonts->h + user_bh;
 	updategeom();
 	sp = sidepad;
 	vp = (topbar == 1) ? vertpad : -vertpad;
