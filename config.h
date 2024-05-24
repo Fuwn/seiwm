79727f6d7b5cfddec7667c012d90314fcb582a4b
Fuwn <contact@fuwn.me>
2024-05-23 17:09:07 -0700

    fix(dwm.c): correct vertical padding on first paint

--- config.h
+++ config.h
@@ -29,7 +29,7 @@ static unsigned int gappov    = 10;       /* vert outer gap between windows and 
 static int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
-static int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
 static int centretitle = 0;
 #define ICONSIZE 16   /* icon size */
 #define ICONSPACING 5 /* space between icon and title */
